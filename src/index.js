24c955c3c6d8923f3cc9928bac9f9187fd5ba10c
Tatsiana Kavalevich <tatsiana.kaval91@gmail.com>
2025-08-07 12:28:42 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-module.exports = function reverse(/* n */) {
-  throw new Error('Not implemented');
+module.exports = function reverse(n) {
+  const strNumber = n.toString();
+  let i = 0;
+  let reverseNum = '';
+  while (i < strNumber.length) {
+    reverseNum = strNumber[i] + reverseNum;
+    i += 1;
+  }
+  if (reverseNum.at(-1) === '-') {
+    reverseNum = reverseNum.slice(0, -1);
+  }
+  return reverseNum;
 };
